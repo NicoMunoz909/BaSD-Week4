31ab02edc76507b150369267915e61221c44e2dc
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-10-06 15:47:48 -0300

    finish excercise 1.1

---
+++ ejercicio-01.js
@@ -0,0 +1,12 @@
+//Ejercico 1 - Variables y Operadores
+
+/*Crear dos variables numéricas y utilizar el operador suma para guardar 
+el valor de la suma de ambos números en una 3er variable.*/
+var num1 = 15;
+var num2 = 31;
+var result = num1+num2;
+
+//Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable
+
+/*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+guardando el resultado de la suma en una 3er variable (utilizar length).*/
