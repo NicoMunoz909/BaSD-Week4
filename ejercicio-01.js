40ed798de465744fec69e0d5167827c38408f44e
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-10-06 15:48:06 -0300

    finish excercise 1.2

--- ejercicio-01.js
+++ ejercicio-01.js
@@ -7,6 +7,9 @@ var num2 = 31;
 var result = num1+num2;
 
 //Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable
+var string1 = 'Hello';
+var string2 = 'World';
+var concatenation = string1+string2;
 
 /*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
 guardando el resultado de la suma en una 3er variable (utilizar length).*/
