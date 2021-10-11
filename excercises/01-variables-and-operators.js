187bc6fafd2c995a93a805053602271e5b52aa36
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-10-11 19:47:44 -0300

    add more logs in ex 01 and 02 to make a line break

--- excercises/01-variables-and-operators.js
+++ excercises/01-variables-and-operators.js
@@ -5,20 +5,26 @@ el valor de la suma de ambos números en una 3er variable.*/
 var num1 = 15;
 var num2 = 31;
 var result = num1+num2;
-console.log('Excercise 1.a')
-console.log(`num1: ${num1} num2: ${num2} result:${result}`)
+console.log('Excercise 1.a');
+console.log(`num1: ${num1}`);
+console.log(`num2: ${num2}`);
+console.log(`result:${result}`);
 
 //Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable
 var string1 = 'Hello';
 var string2 = 'World';
 var concatenation = string1+string2;
-console.log('Excercise 1.b')
-console.log(`string1:${string1} string2:${string2} concatenation:${concatenation}`)
+console.log('Excercise 1.b');
+console.log(`string1:${string1}`);
+console.log(`string2:${string2}`);
+console.log(`concatenation:${concatenation}`);
 
 /*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
 guardando el resultado de la suma en una 3er variable (utilizar length).*/
 var string3 = 'Lorem';
 var string4 = 'Ipsum';
 var combinedLength = string3.length+string4.length;
-console.log('Excercise 1.c')
-console.log(`string1: ${string3} string2: ${string4} result:${combinedLength}`)
+console.log('Excercise 1.c');
+console.log(`string1: ${string3} `);
+console.log(`string2: ${string4}`);
+console.log(`result:${combinedLength}`);
--- excercises/02-strings.js
+++ excercises/02-strings.js
@@ -1,37 +1,42 @@
 // Excercise 02 - Strings
 /* a) - Crear una variable de tipo string con al menos 10 caracteres y 
 convertir todo el texto en mayúscula (utilizar toUpperCase).*/
-var string1 = 'Lorem ipsum dolor sit amet'.toUpperCase();
-var uppercaseString1 = string1;
-console.log('Excercise 2.a')
-console.log(`string:${string1} uppercased string:${uppercaseString1}`)
+var string1 = 'Lorem ipsum dolor sit amet';
+var uppercaseString1 = string1.toUpperCase();
+console.log('Excercise 2.a');
+console.log(`string:${string1}`);
+console.log(`uppercased string:${uppercaseString1}`)
 /* b) - Crear una variable de tipo string con al menos 10 caracteres y 
 generar un nuevo string con los primeros 5 caracteres 
 guardando el resultado en una nueva variable (utilizar substring).*/
 var string2 = 'Lorem ipsum dolor sit amet';
 var substring2 = string2.substring(0,5);
-console.log('Excercise 2.b')
-console.log(`string:${string2} first five characters:${substring2}`)
+console.log('Excercise 2.b');
+console.log(`string:${string2}`);
+console.log(`first five characters:${substring2}`);
 /* c) - Crear una variable de tipo string con al menos 10 caracteres y 
 generar un nuevo string con los últimos 3 caracteres 
 guardando el resultado en una nueva variable (utilizar substring).*/
 var string3 = 'Lorem ipsum dolor sit amet';
 var substring3 = string3.substring(string3.length-3, string3.length);
-console.log('Excercise 2.c')
-console.log(`string:${string3} last three characters:${substring3}`)
+console.log('Excercise 2.c');
+console.log(`string:${string3}`);
+console.log(`last three characters:${substring3}`);
 /* d) - Crear una variable de tipo string con al menos 10 caracteres y 
 generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. 
 Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
 var string4 = 'LOREM IPSUM DOLOR SIT AMET';
 var newString = string4.substring(0,1).toUpperCase()+string4.substring(1).toLowerCase();
-console.log('Excercise 2.d')
-console.log(`string:${string4} capitalized string:${newString}`)
+console.log('Excercise 2.d');
+console.log(`string:${string4}`);
+console.log(`capitalized string:${newString}`);
 /* e) - Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. 
 Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
 var string5 = 'Lorem ipsum dolor sit amet';
 var whitSpaceIndex = string5.indexOf(' ');
-console.log('Excercise 2.e')
-console.log(`string:${string5} white space index:${whitSpaceIndex}`)
+console.log('Excercise 2.e');
+console.log(`string:${string5}`);
+console.log(`white space index:${whitSpaceIndex}`);
 /* f) - Crear una variable de tipo string con al menos 2 palabras largas 
 (10 caracteres y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores 
 para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula 
@@ -42,5 +47,6 @@ var myNewString = string6.substring(0,1).toUpperCase()
 +string6.substring(1, index).toLowerCase()
 +string6.substring(index, index+1).toUpperCase()
 +string6.substring(index+1).toLowerCase();
-console.log('Excercise 2.f')
-console.log(`string:${string6} transformed string:${myNewString}`)
+console.log('Excercise 2.f');
+console.log(`string:${string6}`);
+console.log(`transformed string:${myNewString}`);
