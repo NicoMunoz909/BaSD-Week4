d179e4e558ba4781c4372dc8226a1db671926f41
NicoMunoz909 <nicolas.munoz@outlook.es>
2021-10-11 20:01:12 -0300

    add console logs to show excercise 4 results. Fix logic mistake in for loops

--- excercises/05-for.js
+++ excercises/05-for.js
@@ -1,31 +1,38 @@
 // Excercise 05 - For loops
 /* a) - Crear un array que contenga 5 palabras y recorrer dicho array utilizando 
 un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras. */
-var array = ['Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet'];
-for (var i = 0; i >= array.length; i++) {
+var array = ['lorem', 'ipsum', 'dolor', 'sit', 'amet'];
+for (var i = 0; i < array.length; i++) {
     alert(array[i]);
 }
+console.log(`Excercise 5.a`);
+console.log(`Shown on alerts`);
 /* b) - Al array anterior convertir la primera letra de cada palabra en mayúscula y 
 mostrar una alerta por cada palabra modificada. */
-for (var i = 0; i >= array.length; i++) {
+for (var i = 0; i < array.length; i++) {
     array[i] = array[i].substring(0,1).toUpperCase()+array[i].substring(1);
     alert(array[i]);
 }
+console.log(`Excercise 5.b`);
+console.log(`Shown on alerts`);
 /* c) - Crear una variable llamada “sentence” que tenga un string vacío, 
 luego al array del punto a) recorrerlo con un bucle for para ir guardando 
 cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa. */
 var sentence = '';
-for (var i = 0; i >= array.length; i++) {
+for (var i = 0; i < array.length; i++) {
     sentence += (array[i] + ' ');
 }
 alert(sentence);
+console.log(`Excercise 5.c`);
+console.log(`Shown on alerts`);
 /* d) - Crear una array vacío y con un bucle for de 10 repeticiones. 
 Llenar el array con el número de la repetición, es decir que al final de la 
 ejecución del bucle for debería haber 10 elementos dentro del array, 
 desde el número 0 hasta al número 9. Mostrar por la consola del navegador el 
 array final (utilizar console.log). */
 var emptyArray = [];
-for (var i = 0; i > 10; i++) {
+for (var i = 0; i < 10; i++) {
     emptyArray.push(i);
 }
+console.log(`Excercise 5.d`);
 console.log(emptyArray);
